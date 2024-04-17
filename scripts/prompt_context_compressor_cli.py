f8f4854b139499b15f21b574294c8efccb382924
Shannon Lal <shannonlal@hotmail.com>
2024-04-16 20:47:26 -0400

    Check in working prompt_context compression (#12)

---
+++ scripts/prompt_context_compressor_cli.py
@@ -0,0 +1,41 @@
+"""
+The following is a command line script used to run the rank results function from the llmpromptcompressor package.
+"""
+
+import asyncio
+import argparse
+from typing import List
+from llmpromptcompressor import RankMethodType, settings, get_token_length
+from llmpromptcompressor.llm_prompt_compressor import LLMPromptCompressor
+
+
+async def main(args):
+    context = args.context
+    question = args.question
+    rank_method_type = RankMethodType(args.rank_method)
+    concurrent = args.concurrent
+    target_token = args.target_token
+    llm_api_config = {"open_api_key": settings.OPENAI_API_KEY}
+
+    try:        
+        prompt_context_compressor = LLMPromptCompressor(rank_method=rank_method_type, concurrent_requests=1, llm_api_config=llm_api_config)
+
+        response = await prompt_context_compressor.compress_prompt(
+            context=context,
+            question=question,
+            target_token=target_token)
+        print(f"Compressed Prompt: {response}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Rank context based on a question using LLM.")
+    parser.add_argument("--context", nargs="+", required=True, help="List of context strings")
+    parser.add_argument("--question", type=str, required=True, help="Question string")
+    parser.add_argument("--rank_method", type=str, required=True, help="LLM type (e.g., OPEN_AI)")
+    parser.add_argument("--concurrent", type=int, default=0, help="Number of concurrent requests (default: 0)")
+    parser.add_argument("--target_token", type=float, default=100, help="Target token count (default: 100)")
+
+    args = parser.parse_args()
+
+    asyncio.run(main(args))
